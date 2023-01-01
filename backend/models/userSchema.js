f070f8b029bd15d1ec556c9cd6443997684b4e6b
Manash2712 <manashchauhan2712@gmail.com>
2023-01-01 19:23:17 +0530

    user schema

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,9 +1,36 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Name is required"],
-        maxLength: [50, "Name must be less than 50"],
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+            maxLength: [50, "Name must be less than 50"],
+        },
+        email: {
+            type: String,
+            required: [true, "email is required"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"],
+            minLength: [8, "Password must be atleast 8 characters"],
+            select: false,
+            //by select as false by default this pw field will not come when return user from db.
+        },
+        role: {
+            type: String,
+            enum: Object.values(AuthRoles),
+            default: AuthRoles.USER,
+        },
+        forgotPasswordRoken: String,
+        forgotPasswordExpiry: Date,
     },
-});
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model("User", userSchema);
