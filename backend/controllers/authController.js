2f7912bf218b3033120133195fed08cb1e9dfa7c
Manash2712 <manashchauhan2712@gmail.com>
2023-02-04 23:21:41 +0530

    AuthController - getProfile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -220,3 +220,26 @@ export const resetPassword = asyncHandler(async(req,res)=> {
 })
 
 // TODO: create a controller for change password
+
+
+/*************************
+
+* @GET_PROFILE
+* @REQUEST_TYPE GET
+* @route http://localhost:4000/api/auth/profile
+* @description check for token and populate req.user
+* @parameters 
+* @returns User object
+
+****************************/
+
+export const getProfile = asyncHandler(async(req,_res, next)=> {
+    const {user} = req
+    if(!user){
+        throw new CustomError('User not found', 404)
+    } 
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
